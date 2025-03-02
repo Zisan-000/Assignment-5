edd1f866550e62ff5a11106f33bbd30fa87d6b90
Zisan-000 <zobaer.zisan@gmail.com>
2025-03-02 15:20:55 +0600

    theme

--- styles/event.js
+++ styles/event.js
@@ -138,3 +138,10 @@ function clicked(check){
 document.getElementById('discover').addEventListener('click',function(){
     window.location.href='blog.html';
 })
+
+// theme
+document.getElementById('theme').addEventListener('click',function(){
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16); 
+    console.log(randomColor);
+    document.getElementById('body').style.backgroundColor = randomColor;
+})
